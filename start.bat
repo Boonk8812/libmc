071e6ff1b342afd711243ee29ecfa2e0b90450f8
Boonk3312 <theboonk7@hotmail.com>
2024-02-10 10:22:09 -0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 echo Starting libmc...
-PATH_TO_PYTHON="C:\Program Files\Python39\python.exe" # Update this with your own path
+PATH_TO_PYTHON="C:\Program Files\Python39\python.exe" REM Update this with your own path
 "%PATH_TO_PYTHON%" libmc.py %*
 pause
-echo done loading.
+echo Done loading
