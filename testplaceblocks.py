6644b308644412ad97d451f2aab3f4cf2c42d49a
Boonk3312 <theboonk7@hotmail.com>
2024-02-09 20:34:37 -0800

    Create testplaceblocks.py

---
+++ testplaceblocks.py
@@ -0,0 +1,18 @@
+import random
+from minecraft import Minecraft
+mc = Minecraft.create()
+
+def get_random_block():
+    possible_blocks = ['STONE', 'DIRT', 'GRASS', 'SAND', 'GRAVEL']
+    return random.choice(possible_blocks)
+
+def place_blocks(x, y):
+    width = 30
+    height = 15
+    
+    for row in range(height):
+        for column in range(width):
+            mc.setBlock(x + column, y + row, 'AIR')
+            mc.setBlock(x + column, y + row, get_random_block())
+
+place_blocks(-100, 64) # Change x and z coordinates as desired
